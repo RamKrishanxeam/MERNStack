1e8cb1f65681d48acccdc5e4d278f3417a588afd
Ram Krishan <xeam.designer7@gmail.com>
2025-03-31 17:43:49 +0530

    first commit

---
+++ server/controllers/auth-controllers.js
@@ -0,0 +1,28 @@
+// Controllers Express.js में MVC (Model-View-Controller) आर्किटेक्चर का एक महत्वपूर्ण हिस्सा होते हैं।
+// ये बिजनेस लॉजिक को हैंडल करते हैं और क्लाइंट से आने वाले रिक्वेस्ट (Request) और रिस्पॉन्स (Response) को मैनेज करते हैं।
+
+// बिजनेस लॉजिक वह नियम और प्रोसेस होते हैं, जो यह तय करते हैं कि डेटा को कैसे प्रोसेस और मैनेज किया जाए। उदाहरण के लिए:
+// यूज़र रजिस्ट्रेशन पर ईमेल वेरिफिकेशन भेजना।
+// यूज़र लॉगिन पर पासवर्ड वेरिफिकेशन और टोकन जनरेशन।
+// किसी प्रोडक्ट की कीमत डिस्काउंट के आधार पर बदलना।
+// Sending email verification when a user registers.
+// Validating passwords and generating tokens during login.
+// Applying discounts to product prices based on conditions.
+
+const home = async (req, res) => {
+  try {
+    res.status(200).send("home page");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const registerUser = async (req, res) => {
+  try {
+    res.status(200).send("register");
+  } catch (error) {
+    res.status(400).send("page not found");
+  }
+};
+
+module.exports = { home, registerUser };
---
+++ server/router/auth-router.js
@@ -0,0 +1,15 @@
+// Router Express.js में एक मॉड्यूल होता है जो HTTP रिक्वेस्ट (Request) को हैंडल करता है
+//  और रूट्स (Routes) को मैनेज करने में मदद करता है। यह URL को ऑर्गनाइज़ करने और कोड को साफ-सुथरा रखने में मदद करता है।
+
+const express = require("express");
+const { home, registerUser } = require("../controllers/auth-controllers");
+const router = express.Router();
+
+router.route("/").get(home);
+router.route("/register").get(registerUser);
+
+// router.route("/register").get((req, res) => {
+//   res.status(200).send("register");
+// });
+
+module.exports = router;
