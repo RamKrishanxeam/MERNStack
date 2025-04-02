fd8270650f0889f458bebbc4104b7ea2153c298e
Ram Krishan <xeam.designer7@gmail.com>
2025-04-02 17:14:15 +0530

    working on Validation using ZOD

---
+++ server/validators/user-validator.js
@@ -0,0 +1,30 @@
+const { z } = require("zod");
+
+const signupSchema = z.object({
+  username: z
+    .string({ required_error: "Name is required" })
+    .trim()
+    .min(3, { message: "Name must be at lest of 3 chars." })
+    .max(255, { message: "Name must not be more than 255 characters" }),
+
+  email: z
+    .string({ required_error: "Email is required" })
+    .trim()
+    .email({ message: "invalid email address" })
+    .min(3, { message: "Email must be at least of 3 chars." })
+    .max(255, { message: "Email must not be more than 255 characters" }),
+
+  phone: z
+    .string({ required_error: "Phone is required" })
+    .trim()
+    .min(10, { message: "Phone must be at least of 10 chars." })
+    .max(20, { message: "Phone must not be more than 255 characters" }),
+
+  password: z
+    .string({ required_error: "Password is required" })
+    .trim()
+    .min(7, { message: "Password must be at least of 6 chars." })
+    .max(1024, { message: "Password must not be more than 1024 characters" }),
+});
+
+module.exports = signupSchema;
