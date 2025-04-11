9ec47cfe875cc76a0c69c1b943abb5a52346eabf
Ram Krishan <xeam.designer7@gmail.com>
2025-04-11 16:40:17 +0530

    Admin Authentication: Verifying Whether User is an Admin or Not in our MERN APP

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const adminControllers = require("../controllers/admin-controllers");
 const authMiddleware = require("../middleware/auth-middleware");
-
+const adminMiddleware = require("../middleware/admin-middleware");
 const router = express.Router();
 
-router.route("/users").get(authMiddleware, adminControllers.getAllUsers);
+router
+  .route("/users")
+  .get(authMiddleware, adminMiddleware, adminControllers.getAllUsers);
 router.route("/contact").get(adminControllers.getAllContact);
 
 module.exports = router;
