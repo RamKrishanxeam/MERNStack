0c2c133cd897c80901246a5ec6c4dc89679317d3
Ram Krishan <xeam.designer7@gmail.com>
2025-04-11 16:05:46 +0530

    contact admin done

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const adminControllers = require("../controllers/admin-controllers");
+const authMiddleware = require("../middleware/auth-middleware");
 
 const router = express.Router();
 
-router.route("/users").get(adminControllers.getAllUsers);
+router.route("/users").get(authMiddleware, adminControllers.getAllUsers);
 router.route("/contact").get(adminControllers.getAllContact);
 
 module.exports = router;
